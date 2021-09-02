856e06e4ca204a3ddc1289a4015a9dda651c73f2
DiddySmooth <76023664+DiddySmooth@users.noreply.github.com>
2021-09-02 11:00:12 -0500

    biome map height map precipitation map and landmass map all work seperatly now

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine.Tilemaps;
 
 public class MapGenerator : MonoBehaviour{
 
-    public enum DrawMode {NoiseMap, HeightMap, CitiesMap, BiomesMap};
+    public enum DrawMode {LandmassMap, HeightMap, BiomesMap, PercipitationMap};
     public DrawMode drawMode;
     public int mapWidth;
     public int mapHeight;
@@ -20,7 +20,9 @@ public class MapGenerator : MonoBehaviour{
     public Vector2 offset2;
     public bool autoUpdate;
     [SerializeField] Tilemap terrainTilemap;
-    public TerrainType[] regions;
+    public TerrainType[] biomes;
+    public TerrainType[] heights;
+    public TerrainType[] precipitations;
 
     void Start()
     {
@@ -34,82 +36,194 @@ public class MapGenerator : MonoBehaviour{
 
 
         if(drawMode == DrawMode.BiomesMap){
+            Debug.Log("BiomeMap");
             for (int y = 0; y< mapHeight; y++){
                 for (int x = 0; x < mapWidth; x++){
                     float currentHeight = noiseMap[x,y];
                     float currentPrecipitation = precipitationMap[x,y];
-                    if(currentHeight < .3){
-                        terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[0].tile);
+                    if(currentHeight < .2){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[0].tile);
+                    }
+                    else if(currentHeight < .3){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[13].tile);
                     }
                     else if(currentHeight < .4){
-                        terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[1].tile);
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[1].tile);
                     }
                     else if(currentHeight < .42){
                         if(currentPrecipitation < .8){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[2].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[2].tile);
                         }
                         else if(currentPrecipitation  < 1){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[12].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[12].tile);
                         }
                     }
                     else if(currentHeight < .6){
                         if(currentPrecipitation < .2){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[3].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[3].tile);
                         }
                         else if(currentPrecipitation < .5){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[4].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[4].tile);
                         }
                         else if(currentPrecipitation < .7){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[5].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[5].tile);
                         }
                         else if(currentPrecipitation < .8){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[6].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[6].tile);
                         }
                         else if(currentPrecipitation < 1){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[7].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[7].tile);
                         }
                     }
                     else if(currentHeight <.8){
                         if(currentPrecipitation < .7){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[5].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[5].tile);
                         }
                         else if(currentPrecipitation < .8){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[6].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[6].tile);
                         }
                         else if(currentPrecipitation < 1){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[7].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[7].tile);
                         }
                     }
                     else if(currentHeight < 1){
                         if(currentPrecipitation < .7){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[8].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[8].tile);
                         }
                         else if(currentPrecipitation < .8){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[9].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[9].tile);
                         }
                         else if(currentPrecipitation < 1){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[10].tile);
-                        }
-                    }
-                }
-            }
-            if(drawMode == DrawMode.HeightMap){
-                for (int y = 0; y< mapHeight; y++){
-                    for (int x = 0; x < mapWidth; x++){
-                        float currentHeight = noiseMap[x,y];
-                        if(currentHeight < .3){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[0].tile);
-                        }
-                        else if(currentHeight < .4){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[1].tile);
-                        }
-                        else if(currentHeight < .45){
-                            terrainTilemap.SetTile(new Vector3Int(x,y,0), regions[3].tile);
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), biomes[10].tile);
                         }
                     }
                 }
             }
         }
+        if(drawMode == DrawMode.HeightMap){
+            Debug.Log("HeightMap");
+            for (int y = 0; y< mapHeight; y++){
+                for (int x = 0; x < mapWidth; x++){
+                    float currentHeight = noiseMap[x,y];
+                    if(currentHeight < .1){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[0].tile);
+                    }
+                    else if(currentHeight < .2){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[1].tile);
+                    }
+                    else if(currentHeight < .3){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[2].tile);
+                    }
+                    else if(currentHeight < .4){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[3].tile);
+                    }
+                    else if(currentHeight < .45){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[10].tile);
+                    }
+                    else if(currentHeight < .5){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[4].tile);
+                    }
+                    else if(currentHeight < .6){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[5].tile);
+                    }
+                    else if(currentHeight < .7){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[6].tile);
+                    }
+                    else if(currentHeight < .8){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[7].tile);
+                    }
+                    else if(currentHeight < .9){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[8].tile);
+                    }
+                    else if(currentHeight < 1){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[9].tile);
+                    }
+                }
+            }
+        }
+        if(drawMode == DrawMode.LandmassMap){
+            Debug.Log("LandmassMap");
+            for (int y = 0; y< mapHeight; y++){
+                for (int x = 0; x < mapWidth; x++){
+                    float currentHeight = noiseMap[x,y];
+                    if(currentHeight < .1){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[0].tile);
+                    }
+                    else if(currentHeight < .2){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[1].tile);
+                    }
+                    else if(currentHeight < .3){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[2].tile);
+                    }
+                    else if(currentHeight < .4){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[3].tile);
+                    }
+                    else if(currentHeight < .45){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[10].tile);
+                    }
+                    else if(currentHeight < 1){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[4].tile);
+                    }
+                }
+            }
+        }
+        if(drawMode == DrawMode.PercipitationMap){
+            Debug.Log("LandmassMap");
+            for (int y = 0; y< mapHeight; y++){
+                for (int x = 0; x < mapWidth; x++){
+                    float currentHeight = noiseMap[x,y];
+                    float currentPrecipitation = precipitationMap[x,y];
+                    if(currentHeight < .1){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[0].tile);
+                    }
+                    else if(currentHeight < .2){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[1].tile);
+                    }
+                    else if(currentHeight < .3){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[2].tile);
+                    }
+                    else if(currentHeight < .4){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[3].tile);
+                    }
+                    else if(currentHeight < .45){
+                        terrainTilemap.SetTile(new Vector3Int(x,y,0), heights[10].tile);
+                    }
+                    else if(currentHeight < 1){
+                        if(currentPrecipitation < .1){
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), precipitations[0].tile);
+                        }
+                        else if(currentPrecipitation < .2){
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), precipitations[1].tile);
+                        }
+                        else if(currentPrecipitation < .3){
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), precipitations[2].tile);
+                        }
+                        else if(currentPrecipitation < .4){
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), precipitations[3].tile);
+                        }
+                        else if(currentPrecipitation < .5){
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), precipitations[4].tile);
+                        }
+                        else if(currentPrecipitation < .6){
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), precipitations[5].tile);
+                        }
+                        else if(currentPrecipitation < .7){
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), precipitations[6].tile);
+                        }
+                        else if(currentPrecipitation < .8){
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), precipitations[7].tile);
+                        }
+                        else if(currentPrecipitation < .9){
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), precipitations[8].tile);
+                        }
+                        else if(currentPrecipitation < 1){
+                            terrainTilemap.SetTile(new Vector3Int(x,y,0), precipitations[9].tile);
+                        }
+                    }
+                }
+            }
+        }
+        
         // gameObject.GetComponent<CityGenerator>().GenerateCities(mapWidth, mapHeight);
     }
 
